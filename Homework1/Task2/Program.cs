e397d2dd860081b0cc2832227d4b7d170d6ecb8d
Anthony <gugl382@gmail.com>
2022-11-22 18:37:53 +0300

    Second task completed

--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -7,12 +7,43 @@ int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
 int number3 = int.Parse(Console.ReadLine());
 
-int max = number1;
-int min = number1;
-
-if(number2>max)
+if (number1 == number2 & number1 == number3)
 {
-    max = number2;
+    Console.WriteLine("All numbers are equal");
 }
 
-if(number3>max)
+else
+{
+    int max = number1;
+    int min = number1;
+
+    if (number2 > max)
+    {
+        max = number2;
+    }
+
+    else
+    {
+        min = number2;
+    }
+
+    if (number3 > max)
+    {
+        max = number3;
+    }
+
+    else
+    {
+        if (number3 < min)
+        {
+            min = number3;
+        }
+    }
+
+    Console.Write(max);
+    Console.WriteLine(" is max number");
+
+    Console.Write(min);
+    Console.WriteLine(" is min number");
+}
+
