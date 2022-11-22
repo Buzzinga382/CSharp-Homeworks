86f32ad8745baad9558c44afa807d3cbd1662698
Anthony <gugl382@gmail.com>
2022-11-22 19:01:56 +0300

    Fourth task completed

---
+++ Homework1/Task4/Task4.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Please enter some number");
+
+int number = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while(count <= number)
+{
+    int rest = count % 2;
+    if(rest == 0)
+    {
+        Console.Write(count);
+        Console.Write(" ");
+    }
+    count++;
+}
+Console.WriteLine(" ");
