18d996781d4aa2dc5f03d7bea21547506a806f9e
Anthony <gugl382@gmail.com>
2022-11-22 18:02:03 +0300

    First task completed

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Please enter 2 numbers:");
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write(number1);
+    Console.WriteLine(" bigger");
+
+    Console.Write(number2);
+    Console.WriteLine(" smaller");
+}
+
+else
+{
+    Console.Write(number2);
+    Console.WriteLine(" bigger");
+
+    Console.Write(number1);
+    Console.WriteLine(" smaller");
+}
