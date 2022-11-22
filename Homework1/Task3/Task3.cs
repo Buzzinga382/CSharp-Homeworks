1caf61a9347fe611a7b2fd25e7f183529b410602
Anthony <gugl382@gmail.com>
2022-11-22 18:47:50 +0300

    Third task completed

---
+++ Homework1/Task3/Task3.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка)
+
+Console.WriteLine("Please enter some number");
+
+int number = int.Parse(Console.ReadLine());
+
+int rest = number % 2;
+
+if(rest == 0)
+{
+    Console.WriteLine("This is even number");
+}
+
+else
+{
+    Console.WriteLine("This is odd number");
+}
