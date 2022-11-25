db33841d5246f7aa5d77eb2dcf7b3fcec399733e
Anthony <gugl382@gmail.com>
2022-11-25 21:12:31 +0300

    Task3 completed

---
+++ Homework2/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Please enter number of week day");
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (!isParsed)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+if (number < 1 | number > 7)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+if(number == 6 | number == 7)
+{
+    Console.WriteLine("Yes, it's a day off");
+}
+else
+{
+    Console.WriteLine("No, it's not a day off");
+}
