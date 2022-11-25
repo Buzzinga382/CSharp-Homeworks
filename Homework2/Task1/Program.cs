028e54384e27e6ae3ac221c2b7af078931c1ac42
Anthony <gugl382@gmail.com>
2022-11-25 20:36:44 +0300

    Task1 completed

--- Homework2/Task1/Program.cs
+++ Homework2/Task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное 
+число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Please enter 3-digit number");
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if(!isParsed)
+{
+    Console.WriteLine("Incorrect input");
+}
+
+if(number > 999 | number < 100)
+{
+    Console.WriteLine("Incorrect input");
+}
+
+int thirdDigit = number % 10;
+int secondDigit = ((number - thirdDigit)/10) % 10;
+
+Console.WriteLine($"Second digit is {secondDigit}");
