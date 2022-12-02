65a7d5e0d8d80e4304f6987277751bd7f7ed6a69
Anthony <gugl382@gmail.com>
2022-12-02 17:40:18 +0300

    Homework4 task1 completed

---
+++ Homework4/task1/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Напишите программу, которая принимает на вход два числа (A и B) 
+и метод который возводит число A в натуральную степень B. 
+Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль. 
+Не использовать Math.Pow() */
+
+Console.WriteLine("Please enter two numbers");
+
+bool isParse1 = int.TryParse(Console.ReadLine(), out int num1);
+
+if (!isParse1 || num1 < 0)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+bool isParse2 = int.TryParse(Console.ReadLine(), out int num2);
+
+if (!isParse2 || num2 < 0)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+Console.WriteLine($"{num1} to the power of {num2} equals {PowerOf(num1, num2)}");
+
+
+
+
+
+
+
+// Methods
+
+double PowerOf(int num1, int num2)
+{
+    double result = num1;
+
+    if (num2 == 0)
+    {
+        return 1;
+    }
+
+    if (num2 == 1)
+    {
+        return num1;
+    }
+
+    for (int i = 2; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
