f4f828507afa4c21eb2775ba3101fe7ded1cda79
Anthony <gugl382@gmail.com>
2022-12-03 16:59:08 +0300

    Homework4 task2 modified

---
+++ Homework4/task2_1/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Please enter some number");
+
+bool isParse = int.TryParse(Console.ReadLine(), out int num);
+
+if (!isParse)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+int sum = GetSum(num);
+
+Console.WriteLine($"Sum of digit's of this number is {sum}");
+
+
+
+
+
+
+
+
+// Methods:
+
+int GetSum(int num)
+{
+    int sum = 0;
+    int rest = 0;
+
+    if (num < 0)
+    {
+        num = -num;
+    }
+
+    while(num >= 1)
+    {
+        rest = num % 10;
+        sum = sum + rest;
+        num = num / 10;
+    }
+    return sum;
+}
