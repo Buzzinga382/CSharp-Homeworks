fa81d8e78c40b3d207b6b1ce2d20ec6524b75054
Anthony <gugl382@gmail.com>
2022-12-09 19:36:04 +0300

    Homework6 completed

--- Homework6/task1/Program.cs
+++ Homework6/task1/Program.cs
@@ -1,5 +1,86 @@
-﻿using static Methods.Helper;
+﻿// Пользователь вводит с клавиатуры М чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// 0, 7, 8, -2, -2 -> 2
 
-int[] arr = GenerateArray(5, 15, 6);
 
-PrintArray(arr);
+using static Methods.Helper;
+
+Console.WriteLine("How much numbers you wanna enter?");
+
+bool isParsed = int.TryParse(Console.ReadLine(), out int num);
+
+if (!isParsed || num <= 0)
+{
+    Console.WriteLine("Incorrect input");
+    return;
+}
+
+Console.WriteLine($"Ok, please enter your {num} numbers:");
+
+int[] arrayFromConsole;
+
+try
+{
+    arrayFromConsole = FillArrayFromConsole(num);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+//PrintArray(arrayFromConsole);
+
+int countPositiveNumbers = HowMuchPositiveNumbers(arrayFromConsole);
+
+Console.WriteLine($"You entered {countPositiveNumbers} numbers above zero");
+
+
+
+
+
+
+// Methods:
+
+int HowMuchPositiveNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+int[] FillArrayFromConsole(int num)
+{
+    int[] array = new int[num];
+
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = InputNumber();
+    }
+    return array;
+}
+
+
+
+int InputNumber()
+{
+    bool isParsed1 = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isParsed1)
+    {
+        return number;
+    }
+    else
+    {
+        throw new ArgumentException("That's not a number");
+    }
+}
---
+++ Homework6/task2/Program.cs
@@ -0,0 +1,89 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+using static Methods.Helper;
+
+Console.WriteLine("Please enter b1:");
+double b1;
+try
+{
+    b1 = InputNumber();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+Console.WriteLine("Please enter k1:");
+double k1;
+try
+{
+    k1 = InputNumber();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+Console.WriteLine("Please enter b2:");
+double b2;
+try
+{
+    b2 = InputNumber();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+Console.WriteLine("Please enter k2:");
+double k2;
+try
+{
+    k2 = InputNumber();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+if (k1 == k2 && b1 != b2)
+{
+    Console.WriteLine("Lines are parallel");
+    return;
+}
+
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("Lines coincide");
+    return;
+}
+
+double[] arr = IntersectionPoint(b1, k1, b2, k2);
+
+Console.Write($"The intersection point is ");
+PrintArray(arr);
+
+
+
+
+
+// Method:
+
+double[] IntersectionPoint(double b1, double k1, double b2, double k2)
+{
+    double[] array = new double[2];
+    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+    double y = Math.Round(k1 * (b2 - b1) / (k1 - k2) + b1, 2);
+
+    array[0] = x;
+    array[1] = y;
+
+    return array;
+}
+
