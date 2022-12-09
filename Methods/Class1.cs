dd1723bfe1c00ca0f9e7017f70a015a8ff633da2
Anthony <gugl382@gmail.com>
2022-12-09 17:12:00 +0300

    Methods added

---
+++ Methods/Class1.cs
@@ -0,0 +1,24 @@
+﻿namespace Methods;
+
+public class Helper
+{
+    public static int[] GenerateArray(int min, int max, int length)
+    {
+        Random random = new Random();
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = random.Next(min, max + 1);
+        }
+        return array;
+    }
+
+    public static void PrintArray(int[] array)
+    {
+        string message = string.Join(", ", array);
+        Console.WriteLine($"[{message}]");
+    }
+
+
+}
