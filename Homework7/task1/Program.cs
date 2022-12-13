aba1217f8e8df4ab0880cb47983e59d7353c9453
Anthony <gugl382@gmail.com>
2022-12-13 09:46:38 +0300

    Homework7 created

---
+++ Homework7/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+double test = InputDoubleNumber("Please enter some number:");
+
+Console.WriteLine(test);
+
+
+
+double InputDoubleNumber(string str)
+    {
+        Console.WriteLine(str);
+        
+        bool isParsed = double.TryParse(Console.ReadLine(), out double number);
+
+        if (isParsed)
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect input...");
+            return InputDoubleNumber(str);
+        }
+    }
